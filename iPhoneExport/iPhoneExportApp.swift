74b86afbfaca865e7a3a795e35f01fa9cc0f2033
Eduard-2021 <sergsim1978@gmail.com>
2025-11-07 15:33:13 +0200

    Initial Commit

---
+++ iPhoneExport/iPhoneExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneExportApp.swift
+//  iPhoneExport
+//
+//  Created by Macintosh HD on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
